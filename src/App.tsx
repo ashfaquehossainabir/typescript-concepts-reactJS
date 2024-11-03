3886a7b3b403a564c95a3d94f5e0c632f5416b1a
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2024-11-03 19:14:32 +0600

    Uploaded

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// CSS Source
+import './App.css'
+
+// Components
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <div>
+          <Header title='Hello World' color='red'/>
+      </div>
+    </>
+  )
+}
+
+export default App
